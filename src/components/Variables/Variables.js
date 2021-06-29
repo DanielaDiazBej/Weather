d320c6b2df87a3303daadedd363458f043e08af9
Daniela Diaz Bejarano <60675531+DanielaDiazBej@users.noreply.github.com>
2021-06-28 19:56:31 -0500

    Update Variables.js

--- src/components/Variables/Variables.js
+++ src/components/Variables/Variables.js
@@ -1,17 +1,3 @@
 import React from 'react';
-import { arrayVar } from './../../const/arrayVar';
 
-const Variables = () => {
 
-    const [arrayVar, set] =  useState({
-        iconVar:''
-    })
-
-    return(
-        <div className="d-flex">
-            <img src= ""/>
-
-            <p></p>
-        </div>
-    );
-}
